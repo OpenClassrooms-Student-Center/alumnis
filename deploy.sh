6d9f3cd2cdf7491df62f8a2fd792185fd8d5489b
Céline MS <celine.martinet.sanchez+github@gmail.com>
2016-11-02 17:00:37 +0100

    Bash script changed

--- deploy.sh
+++ deploy.sh
@@ -20,7 +20,7 @@ REPO=`git config remote.origin.url`
 SSH_REPO=${REPO/https:\/\/github.com\//git@github.com:}
 SHA=`git rev-parse --verify HEAD`
 
-# Clone the existing gh-pages for this repo into output/
+# Clone the existing gh-pages for this repo into out/
 # Create a new empty branch if gh-pages doesn't exist yet (should only happen on first deply)
 git clone $REPO out
 cd out
@@ -28,7 +28,7 @@ git checkout $TARGET_BRANCH || git checkout --orphan $TARGET_BRANCH
 cd ..
 
 # Clean out existing contents
-rm -rf output/**/* || exit 0
+rm -rf out/**/* || exit 0
 
 # Run our compile script
 doCompile
