77a450b5fcfb575b22a2d2ac3b8c36cc87fb1e47
tom.gabriele <tom.gabriele@onysos.fr>
2018-03-27 16:13:26 +0200

    Add a method to replace empty or non-existent students images with a generic one.

--- plugins/pelican-md-metayaml/md_metayaml.py
+++ plugins/pelican-md-metayaml/md_metayaml.py
@@ -1,7 +1,14 @@
+import os
+
 from pelican import signals
 from pelican.readers import MarkdownReader
 
 from .markdown_metayaml.meta_yaml import MetaYamlExtension
+
+PROJECT_ROOT_DIR = os.path.dirname(os.path.dirname(os.path.dirname(__file__)))
+STUDENTS_PICTURE_DIR = os.path.join(PROJECT_ROOT_DIR, "content/images/students")
+GENERIC_IMAGE = 'oc-logo.svg'
+
 
 class MarkdownYAMLReader(MarkdownReader):
     """Reader for Markdown files with YAML metadata"""
@@ -18,12 +25,28 @@ class MarkdownYAMLReader(MarkdownReader):
         for x in ("tags", "authors"):
             if x in meta:
                 meta[x] = [",".join(meta[x])]
-
+        meta = self.replace_empty_pictures_with_generic(meta)
         return super(MarkdownYAMLReader, self)._parse_metadata(meta)
+
+    @staticmethod
+    def replace_empty_pictures_with_generic(meta):
+        """
+        Replace non existant images of students with a generic one
+        :param meta: a student meta data
+        :return: modified (or not) meta data with generic picture
+        """
+        student_picture = os.path.join(STUDENTS_PICTURE_DIR, meta['image'][0])
+        if not os.path.exists(student_picture) or meta['image'][0].strip() == '':
+            print("Students picture %s doesn't exists. It will be replaced by generic image %s." %
+                  (meta["image"][0], GENERIC_IMAGE))
+            meta["image"] = [GENERIC_IMAGE]
+        return meta
+
 
 def add_reader(readers):
     for k in MarkdownYAMLReader.file_extensions:
         readers.reader_classes[k] = MarkdownYAMLReader
+
 
 def register():
     signals.readers_init.connect(add_reader)
